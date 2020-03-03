8c9f7d1cade1724d69116d4de421f3045dab742d
Seonpyo Kim <spkim@kodebox.io>
2020-03-03 20:50:29 +0900

    Get X25519 public key from private key

--- src/key/keyExchange.ts
+++ src/key/keyExchange.ts
@@ -23,3 +23,14 @@ export const exchange = (
     const sharedSecret = nacl.scalarMult(scalar, groupElement);
     return toHex(sharedSecret);
 };
+
+/**
+ * Gets the X25519 public key(on Curve25519) for a private key
+ * @param x25519Private 32 byte hexadecimal string of a secret key
+ * @returns 32 byte hexadecimal string of the public key
+ */
+export const x25519GetPublicFromPrivate = (x25519Private: string): string => {
+    const scalar = toArray(x25519Private);
+    const x25519Public = nacl.scalarMult.base(scalar);
+    return toHex(x25519Public);
+};
